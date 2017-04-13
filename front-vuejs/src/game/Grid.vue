af476360e97df28b8d47cec64b2d92cb5fe6fc0b
Mathieu <mat.guillotin@gmail.com>
2017-04-13 10:12:08 +0200

    optimized stone

--- front-vuejs/src/game/Grid.vue
+++ front-vuejs/src/game/Grid.vue
@@ -2,7 +2,15 @@
   <svg>
     <line v-for="x in centers" :x1="pointToCoordinate(x)" :x2="pointToCoordinate(x)" :y1="pointToCoordinate(0)" :y2="pointToCoordinate(18)" stroke="black" stroke-width="1"></line>
     <line v-for="y in centers" :y1="pointToCoordinate(y)" :y2="pointToCoordinate(y)" :x1="pointToCoordinate(0)" :x2="pointToCoordinate(18)" stroke="black" stroke-width="1"></line>
+    <circle cx="70" cy="70" r="4"></circle>
+    <circle cx="190" cy="70" r="4"></circle>
+    <circle cx="310" cy="70" r="4"></circle>
+    <circle cx="70" cy="190" r="4"></circle>
     <circle cx="190" cy="190" r="4"></circle>
+    <circle cx="310" cy="190" r="4"></circle>
+    <circle cx="70" cy="310" r="4"></circle>
+    <circle cx="190" cy="310" r="4"></circle>
+    <circle cx="310" cy="310" r="4"></circle>
   </svg>
 </template>
 
